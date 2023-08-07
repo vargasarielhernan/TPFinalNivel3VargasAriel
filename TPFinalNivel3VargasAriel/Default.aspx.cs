82849a4b04f8ba08a859b14c238c74d79080bb75
Your Name <you@example.com>
2023-08-07 00:52:32 -0300

    card en pagina defauld

--- TPFinalNivel3VargasAriel/Default.aspx.cs
+++ TPFinalNivel3VargasAriel/Default.aspx.cs
@@ -1,16 +1,23 @@
-﻿using System;
+﻿using negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
 
 namespace TPFinalNivel3VargasAriel
 {
     public partial class Home : System.Web.UI.Page
     {
+        public List<Articulos> Productos { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            ListaArticulos lista = new ListaArticulos();
+            Productos = lista.ListarSP();
+            Repetidor.DataSource = Productos;
+            Repetidor.DataBind();
 
         }
     }
